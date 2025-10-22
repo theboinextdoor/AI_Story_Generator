e40f82a844f1d63f5960c8b411c7568e6364d0a0
theboynextdoor <cman55245@gmail.com>
2025-10-22 23:31:36 +0530

    Added story schemas

--- models/story.py
+++ models/story.py
@@ -8,3 +8,22 @@ class Story(Base):
     __tablename__ = "stories"
 
     id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    session_id = Column(String, index=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    nodes = relationship("StoryNode", back_populate="story")
+
+
+class StoryNode(Base):
+    __tablename__ = "story_nodes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    story_id = Column(Integer, ForeignKey("stories.id"), index=True)
+    content = Column(String)
+    is_root = Column(Boolean, default=False)
+    id_ending = Column(Boolean, default=False)
+    is_winning_ending = Column(Boolean, default=False)
+    options = Column(JSON, default=list)
+
+    story = relationship("Story", back_populates="nodes")
