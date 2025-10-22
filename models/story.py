d0ab49743f7d84f042906a91c3e990faedf916d7
theboynextdoor <cman55245@gmail.com>
2025-10-22 16:24:09 +0530

    removed the .env file

--- models/story.py
+++ models/story.py
@@ -1,7 +1,10 @@
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime, JSON
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
+from db.database import Base
 
 
-class Story:
-    pass
+class Story(Base):
+    __tablename__ = "stories"
+
+    id = Column(Integer, primary_key=True, index=True)
